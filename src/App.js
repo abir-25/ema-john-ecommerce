419cb66656a0944d889d1ce9922e044f218dfe74
abir-25 <abirbgc25@gmail.com>
2021-10-03 18:20:09 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Header></Header>
-      <Shop></Shop>
+    <div className="App">
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
